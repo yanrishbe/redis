552a200182d25547f5f91113767229c0ec227718
yanrishbe <yana.strabuk@gmail.com>
2019-01-05 12:13:30 +0300

    "Rename main.go into server.go"

--- main/server.go
+++ main/server.go
@@ -18,7 +18,7 @@ type command struct {
 }
 
 type jsonKeyValue struct {
-	key string
+	key   string
 	value string
 }
 
@@ -187,7 +187,7 @@ func main() {
 	flag.StringVar(&mode, "m", defaultMode, "shorthand for --mode")
 	flag.Parse()
 
-	pattern:="^(6553[0-5]|655[0-2]\\d|65[0-4](\\d){2}|6[0-4](\\d){3}|[1-5](\\d){4}|[1-9](\\d){0,3})$"
+	pattern := "^(6553[0-5]|655[0-2]\\d|65[0-4](\\d){2}|6[0-4](\\d){3}|[1-5](\\d){4}|[1-9](\\d){0,3})$"
 	match, err := regexp.MatchString(pattern, port)
 	if !match || err != nil {
 		log.Fatalln(func() error {
